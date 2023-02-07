7d2cf436ab987c18f648ff561ba5d8ee012f2886
peruso <michaelyuta@live.jp>
2023-02-07 02:52:12 -0500

    ch12

--- config/settings.py
+++ config/settings.py
@@ -154,6 +154,9 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
+
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 
