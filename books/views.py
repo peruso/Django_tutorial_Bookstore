8c5014a20e5a2519723184101aeadeeb9af6bf78
peruso <michaelyuta@live.jp>
2023-02-15 14:49:19 -0500

    ch14

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
@@ -16,3 +17,18 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"  # これを見るためにはpermissionがいるという意味だろう
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+    # queryset = Book.objects.filter(title__icontains="beginners")
+    def get_queryset(
+        self,
+    ):
+        # 呼び出してないけどこの関数、自動で実行されているのだな。。。。なぜ.そもそもなぜ関数にする必要があるのだろう。上のやつに加えるじゃダメなんかね。。試したらできた！！どっちでもOK
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
